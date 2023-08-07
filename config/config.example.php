b6df2e7a17d3ce7a98e6bf6466c61a446986a1d3
yusam <yusam@yandex.ru>
2023-08-07 14:57:34 +0300

    #init

--- config/config.example.php
+++ config/config.example.php
@@ -6,5 +6,9 @@ return [
         'baseUrl' => 'https://api.telegram.org',
         'storageLogFile' => __DIR__ . '/../tmp/curl-ext-debug-telegram-sdk.log',
         'token' => '',
+        'botName' => '',
+        'ownerUserChatId' => 0,
+        'chatId_1' => 0,
+        'chatId_2' => 0,
     ],
 ];
